202f70be2e2c90ff010af624c9ccb662d3ad5b24
Sanjeev Kumar Pandey <sanpande@adobe.com>
2019-04-28 12:40:12 +0530

    init commit . version dev 0.0

---
+++ app/src/main/java/com/example/sanjeev/exoplayer_androidplayer/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.sanjeev.exoplayer_androidplayer;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startPlayback(View view){
+        Intent intent = new Intent(this, VideoPlayerActivity.class);
+//        EditText editText = (EditText) findViewById(R.id.editText);
+//        String message = editText.getText().toString();
+//        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
+    }
+
+}
---
+++ app/src/main/java/com/example/sanjeev/exoplayer_androidplayer/VideoPlayerActivity.java
@@ -0,0 +1,223 @@
+package com.example.sanjeev.exoplayer_androidplayer;
+
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.Format;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.extractor.ExtractorsFactory;
+import com.google.android.exoplayer2.source.AdaptiveMediaSourceEventListener;
+import com.google.android.exoplayer2.source.TrackGroupArray;
+import com.google.android.exoplayer2.source.hls.HlsMediaSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveVideoTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.PlaybackControlView;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSource;
+import com.google.android.exoplayer2.upstream.DataSpec;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
+
+import java.io.IOException;
+
+public class VideoPlayerActivity extends AppCompatActivity implements ExoPlayer.EventListener {
+
+    private SimpleExoPlayerView playerView;
+    //private EditText contentUrl;
+    private String mediaUrl;
+    private SimpleExoPlayer player;
+    private ProgressBar progressBar;
+    private Button fullScreenBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_player);
+
+        // 1. Create a default TrackSelector
+        Handler mainHandler = new Handler();
+        BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+        TrackSelection.Factory videoTrackSelectionFactory = new AdaptiveVideoTrackSelection.Factory(bandwidthMeter);
+        TrackSelector trackSelector = new DefaultTrackSelector(videoTrackSelectionFactory);
+
+        // 2. Create a default LoadControl
+        LoadControl loadControl = new DefaultLoadControl();
+
+
+        // 3. Create the player
+        player = ExoPlayerFactory.newSimpleInstance(this, trackSelector, loadControl);
+
+        playerView = (SimpleExoPlayerView) findViewById(R.id.player_view);
+        playerView.setPlayer(player);
+
+        // Measures bandwidth during playback. Can be null if not required.
+        DefaultBandwidthMeter defaultBandwidthMeter = new DefaultBandwidthMeter();
+        // Produces DataSource instances through which media data is loaded.
+        DefaultHttpDataSource.Factory dataSourceFactory = new DefaultHttpDataSourceFactory(Util.getUserAgent(this, "Exo2"), defaultBandwidthMeter);
+        //this,
+        //        Util.getUserAgent(this, "Exo2"), defaultBandwidthMeter);
+        // Produces Extractor instances for parsing the media data.
+        ExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
+
+        //contentUrl = (EditText) findViewById(R.id.contentUrlText);
+        mediaUrl = getResources().getString(R.string.contentUrl);//contentUrl.getText().toString();
+
+        // This is the MediaSource representing the media to be played.
+        HlsMediaSource hlsMediaSource = new HlsMediaSource(Uri.parse(mediaUrl), dataSourceFactory, mainHandler, new AdaptiveMediaSourceEventListener() {
+            @Override
+            public void onLoadStarted(DataSpec dataSpec, int dataType, int trackType, Format trackFormat, int trackSelectionReason, Object trackSelectionData, long mediaStartTimeMs, long mediaEndTimeMs, long elapsedRealtimeMs) {
+
+            }
+
+            @Override
+            public void onLoadCompleted(DataSpec dataSpec, int dataType, int trackType, Format trackFormat, int trackSelectionReason, Object trackSelectionData, long mediaStartTimeMs, long mediaEndTimeMs, long elapsedRealtimeMs, long loadDurationMs, long bytesLoaded) {
+
+            }
+
+            @Override
+            public void onLoadCanceled(DataSpec dataSpec, int dataType, int trackType, Format trackFormat, int trackSelectionReason, Object trackSelectionData, long mediaStartTimeMs, long mediaEndTimeMs, long elapsedRealtimeMs, long loadDurationMs, long bytesLoaded) {
+
+            }
+
+            @Override
+            public void onLoadError(DataSpec dataSpec, int dataType, int trackType, Format trackFormat, int trackSelectionReason, Object trackSelectionData, long mediaStartTimeMs, long mediaEndTimeMs, long elapsedRealtimeMs, long loadDurationMs, long bytesLoaded, IOException error, boolean wasCanceled) {
+
+            }
+
+            @Override
+            public void onUpstreamDiscarded(int trackType, long mediaStartTimeMs, long mediaEndTimeMs) {
+
+            }
+
+            @Override
+            public void onDownstreamFormatChanged(int trackType, Format trackFormat, int trackSelectionReason, Object trackSelectionData, long mediaTimeMs) {
+
+            }
+        });
+
+        player.addListener(this);
+        player.prepare(hlsMediaSource);
+        playerView.requestFocus();
+        player.setPlayWhenReady(true);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        fullScreenBtn = (Button) findViewById(R.id.fullScreenButton);
+
+        //attching eventlistener
+        fullScreenBtn.setOnClickListener(fullScreenExeperience);
+    }
+
+    @Override
+    public void onTimelineChanged(Timeline timeline, Object manifest) {
+
+    }
+
+    @Override
+    public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
+
+    }
+
+    @Override
+    public void onLoadingChanged(boolean isLoading) {
+
+    }
+
+    @Override
+    public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+
+        switch (playbackState) {
+            case ExoPlayer.STATE_BUFFERING:
+                //You can use progress dialog to show user that video is preparing or buffering so please wait
+                progressBar.setVisibility(View.VISIBLE);
+                break;
+            case ExoPlayer.STATE_IDLE:
+                //idle state
+                break;
+            case ExoPlayer.STATE_READY:
+                // dismiss your dialog here because our video is ready to play now
+                progressBar.setVisibility(View.GONE);
+                break;
+            case ExoPlayer.STATE_ENDED:
+                // do your processing after ending of video
+                break;
+        }
+    }
+
+    @Override
+    public void onPlayerError(ExoPlaybackException error) {
+
+        AlertDialog.Builder adb = new AlertDialog.Builder(VideoPlayerActivity.this);
+        adb.setTitle("Unable To Stream Video");
+        adb.setMessage("It seems that something's gone wrong.\nPlease try again.\nVideo Url:\n" + getString(R.string.contentUrl));
+        adb.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                finish(); // take out user from this activity. you can skip this
+            }
+        });
+        AlertDialog ad = adb.create();
+        ad.show();
+    }
+
+    @Override
+    public void onPositionDiscontinuity() {
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (player != null) {
+            player.setPlayWhenReady(false); //to pause a video because now our video player is not in focus
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        player.release();
+    }
+
+    @SuppressLint("InlinedApi")
+    public final View.OnClickListener fullScreenExeperience = new View.OnClickListener() {
+        public void onClick(View view) {
+            boolean fullScreen = false;
+            if (fullScreen) {
+                playerView.setSystemUiVisibility(View.SYSTEM_UI_LAYOUT_FLAGS
+                        | View.SYSTEM_UI_FLAG_VISIBLE
+                        | View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR);
+                fullScreen = false;
+            } else {
+                playerView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+                fullScreen = true;
+            }
+        }
+    };
+}
